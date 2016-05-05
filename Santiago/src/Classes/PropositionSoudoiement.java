7b66f443b7d55647f28974dfdbc89c33e0d1e993
Moona-AK <aminetou_a.k@hotmail.fr>
2016-05-05 13:35:19 +0200

    proposition et soutien soudoiement

--- Santiago/src/Classes/PropositionSoudoiement.java
+++ Santiago/src/Classes/PropositionSoudoiement.java
@@ -1,5 +1,33 @@
 package Classes;
 
-public class ProposerSoudoiement {
+public class PropositionSoudoiement {
 
-}
+	int idPS;
+	int montant;
+ 	Joueur premier; //nom du premier qui propose ce soudoiement
+	Fosse f; // fossé sujet du soudoiement.
+	ArrayList<SoutienSoudoiement> supporters; //comprend les joueurs qui soutienenent ce soudoiement. est vide à la base
+	boolean etat; //true: en cours : proposition faite à ce tour , fa : passée : proposition archive : un autre joueur ne peut la soutenir.
+	
+	public PropositionSoudoiement(int idPS,Joueur j,int m, Fosse f){
+		this.idPS=idPS;
+		this.premier=j;
+		this.montant=m;
+		this.f=f;
+		this.supporters=new ArrayList<SoutienSoudoiement>();
+		this.etat=true;
+	}
+	
+	
+	
+	
+	public int getIdPS() {
+		return idPS;
+	}
+
+
+
+
+	public void setIdPS(int idPS) {
+		this.idPS = idPS;
+	}
